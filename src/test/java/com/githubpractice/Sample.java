b93f8698bd9a04f84caf98f49223e86eee0da077
krish <krish@Gopalakrishnan>
2023-09-23 22:42:56 +0530

    First TestNg project to github

---
+++ src/test/java/com/githubpractice/Sample.java
@@ -0,0 +1,31 @@
+package com.githubpractice;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.amazon.in/");
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		boolean status = js.executeScript("return document.readyState").toString().equals("complete");
+
+		if (status) {
+			System.out.println("page load completely");
+		} else {
+			System.out.println("page load incomplete");
+		}
+		
+		
+		driver.quit();
+	}
+}
